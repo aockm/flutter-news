4d081e326c374954afcfb0a1889d0710fd24a02b
aockm <3504747913@qq.com>
2025-05-23 02:38:42 +0800

    设计稿适配、加入图片字体资源、欢迎界面

--- lib/pages/welcome/welcome_page.dart
+++ lib/pages/welcome/welcome_page.dart
@@ -1,26 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/common/utils/utils.dart';
+import 'package:flutter_news/common/values/values.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
+
+  
+  
 
   @override
   State<WelcomePage> createState() => _WelcomePageState();
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  /// 页头标题
+  Widget _buildPageHeadTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: duSetHeight(60)),
+      child: Text(
+        "Features",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontFamily: "Montserrat",
+          fontWeight: FontWeight.w600,
+          fontSize: duSetFontSize(24),
+        ),
+      ),
+    );
+  }
+  
+   /// 页头说明
+  Widget _buildPageHeaderDetail() {
+    return Container(
+      width: duSetWidth(242),
+      height: duSetHeight(70),
+      margin: EdgeInsets.only(top: duSetHeight(14)),
+      child: Text(
+        "The best of news channels all in one place. Trusted sources and personalized news for you.",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontFamily: "Avenir",
+          fontWeight: FontWeight.normal,
+          fontSize: duSetFontSize(16),
+          height: 1.3,
+        ),
+      ),
+    );
+  }
+   /// 特性说明
+  Widget _buildFeatureItem(String imageName, String intro, double marginTop) {
+    return Container(
+      width: duSetWidth(295),
+      height: duSetHeight(80),
+      margin: EdgeInsets.only(top: duSetHeight(marginTop)),
+      child: Row(
+        children: [
+          SizedBox(
+            width: duSetWidth(80),
+            height: duSetWidth(80),
+            child: Image.asset(
+              "assets/images/$imageName.png",
+              fit: BoxFit.none,
+            ),
+          ),
+          Spacer(),
+          SizedBox(
+            width: duSetWidth(195),
+            child: Text(
+              intro,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: AppColors.primaryText,
+                fontFamily: "Avenir",
+                fontWeight: FontWeight.normal,
+                fontSize: duSetFontSize(16),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+   /// 开始按钮
+  Widget _buildStartButton() {
+    return Container(
+      width: duSetWidth(295),
+      height: duSetHeight(44),
+      margin: EdgeInsets.only(bottom: duSetHeight(20)),
+      child: TextButton(
+        style: TextButton.styleFrom(
+           backgroundColor: AppColors.primaryElement,
+           foregroundColor: AppColors.primaryElementText, 
+           shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(duSetWidth(6))),
+        ),
+        ),
+       
+        child: Text("Get started"),
+        
+        onPressed: () {},
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     // 高度去掉 顶部、底部 导航
     ScreenUtil.init(context,
     designSize: Size(375, 812 - 44 - 34));
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter news"),
-      ),
+      
       body: Center(
-        child: Container(
-          child: Text("Hello world"),
+        child: Column(
+          children: <Widget>[
+            _buildPageHeadTitle(),
+            _buildPageHeaderDetail(),
+            _buildFeatureItem(
+              "feature-1",
+              "Compelling photography and typography provide a beautiful reading",
+              86,
+            ),
+             _buildFeatureItem(
+              "feature-2",
+              "Sector news never shares your personal data with advertisers or publishers",
+              40,
+            ),
+            _buildFeatureItem(
+              "feature-3",
+              "You can get Premium to unlock hundreds of publications",
+              40,
+            ),
+            Spacer(),
+            _buildStartButton(),
+          ],
         ),
       ),
     );
